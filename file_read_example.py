db070ec697d61b7c0701a6e9994144ab35521961
Bogdan Ratiu <bratiu-dn@drivenets.com>
2023-03-10 16:45:52 +0100

    Session 8 examples

---
+++ file_read_example.py
@@ -0,0 +1,21 @@
+import requests
+
+# the simplest way to work with a file!
+fp = open("text.txt")
+print(fp.read())
+fp.close()
+
+fp = open("text.txt")
+
+# files are iterable!!
+for line in fp:
+    # print(line, end="")
+    # print(line[:-1])
+    print(line.rstrip())
+
+r = requests.get("https://www.gutenberg.org/cache/epub/345/pg345.txt")
+fp = open("dracula.txt", "w")
+fp.write(r.text)
+
+
+
